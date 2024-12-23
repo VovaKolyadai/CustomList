3c87db97ab4382897e27054d6c3a9a13f20d23ff
VovaKolyadai <medzhigam.games@gmail.com>
2024-12-23 15:44:30 +0100

    implement GetEnumerator

--- CustomList/CustomList/Program.cs
+++ CustomList/CustomList/Program.cs
@@ -4,6 +4,13 @@
     {
         static void Main(string[] args)
         {
+            List<string> list = new List<string>();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Remove("2");
+            list.Remove("3");
+            list.Add("4");
             Console.WriteLine("Hello, World!");
         }
     }
---
+++ CustomList/CustomList/PuppyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    internal class PuppyList<T> : IEnumerable<T>
+    {
+        private T[] _data;
+        private int _size;
+        public int Capacity
+        {
+            get { return _data.Length; }
+            set
+            {
+                if(value < _size)
+                {
+
+                }
+            }
+        }
+        //TODO
+        public void Add(T element)
+        {
+            var temp = _data;
+            _data = new T[temp.Length * 2];
+            _data = temp;
+        }
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)_data).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+        
+    }
+}
