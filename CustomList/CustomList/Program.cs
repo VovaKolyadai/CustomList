8aa3b2d741390afe7a59a4032dede464c2764767
VovaKolyadai <medzhigam.games@gmail.com>
2024-12-25 14:35:03 +0100

    event added and fixed

--- CustomList/CustomList/Program.cs
+++ CustomList/CustomList/Program.cs
@@ -8,18 +8,27 @@
             {
                 1, 22, 57, 100, 0
             };
+            puppyList.ExpandedEvent += Grow;
+
             puppyList.Where(x => x > 1).ToList().ForEach(Console.WriteLine);
             Console.WriteLine("-----");
             var sortedPuppyList = puppyList.OrderBy(x => x).ToList();
             sortedPuppyList.ForEach(Console.WriteLine);
             Console.WriteLine("-----");
-
+            for (int i = 0; i < 100; i++)
+            {
+                puppyList.Add(1);
+            }
 
             var list = new List<string>()
            {
                "str", "dd", "fff"
            };
-            list.ForEach(p => Console.WriteLine(p));
+            list.ForEach(Console.WriteLine);
+        }
+        public static void Grow(object sender, EventArgs e)
+        {
+            Console.WriteLine("GROOOOW");
         }
     }
 }
